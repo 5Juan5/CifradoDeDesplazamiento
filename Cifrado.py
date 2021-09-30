f519e886800a5739238b74f7103098a2866d101b
Juan Antonio <juan_antonio_torres.sic@tesco.edu.mx>
2021-09-30 14:24:54 -0500

    Se agrego sin hacer pruebas el cifrado

--- Cifrado.py
+++ Cifrado.py
@@ -2,11 +2,17 @@ from Datos import *
 class Cifrado(Datos):
     def __init__(self, palabra, clave, abc):
         self.palabra = palabra
-        self.clave
+        self.clave = clave
+        self.abc = abc
         super().__init__(palabra, clave, abc)
 
     def cifrar(self):
-        pass
+        for i in range(len(self.palabra)):
+            for j in range (len(self.abc)):
+                if self.palabra[i] in self.abc[j] :
+                    resultado = self.abc[(self.palabra[i]+self.abc[j]) %len(self.abc)]
+        return resultado
+
 
 
 
