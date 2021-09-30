5f2d98e16a24d1f5046e841cb77813d912b045d2
Juan Antonio <juan_antonio_torres.sic@tesco.edu.mx>
2021-09-30 13:22:32 -0500

    Se guarda primera parte del proyecto

---
+++ Cifrado.py
@@ -0,0 +1,13 @@
+from Datos import *
+class Cifrado(Datos):
+    def __init__(self, palabra, clave, abc):
+        self.palabra = palabra
+        self.clave
+        super().__init__(palabra, clave, abc)
+
+    def cifrar(self):
+        pass
+
+
+
+
