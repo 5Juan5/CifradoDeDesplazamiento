7e11505168cc66e6e27850c2146b5a4d77cebf92
Juan Antonio <juan_antonio_torres.sic@tesco.edu.mx>
2021-10-04 02:06:06 -0500

    Se termino el cifrado falta hacer descifrado

--- Cifrado.py
+++ Cifrado.py
@@ -7,10 +7,11 @@ class Cifrado(Datos):
         super().__init__(clave, palabra)
 
     def cifrar(self):
-        for i in range(len(self.palabra)):
-            for j in range (len(self.abc)):
-                if self.palabra[i] in self.abc[j] :
-                    resultado = self.abc[(self.palabra[i]+self.abc[j]) %len(self.abc)]
+        resultado = ''
+        for i in self.palabra:
+            for j in self.abc:
+                if i in j:
+                    resultado += self.abc[(self.abc.index(j)+self.clave)% len(self.abc)]
         return resultado
 
 
