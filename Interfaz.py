fe56f1b14da902972073f7b9f8dd4c26faaaffec
Juan <juan_antonio_torres.sic@tesco.edu.mx>
2021-10-20 00:16:31 -0500

    segunda parte

--- Interfaz.py
+++ Interfaz.py
@@ -1,3 +1,5 @@
 print('se empezara aqui la interfaz con lo nuevo del curso')
 class interfaz():
   print('aun no pongo nada referente a la interfaz, solo hago pruebas de git y github')
+  def __init__(self):
+    pass
